14235d9808fb0f1205f2b76bb93928953850d7d7
MCUPO <MCUPO@e0e46c49-be69-4f5a-ad62-21024a331aea>
2012-05-14 20:49:13 +0000

    Manejo de eventos para operar.

--- Java/T6/TP2/src/ar/edu/ort/calculadora/CalculadoraGrafica.java
+++ Java/T6/TP2/src/ar/edu/ort/calculadora/CalculadoraGrafica.java
@@ -5,39 +5,41 @@ import java.awt.Color;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.util.HashMap;
 import java.util.Observable;
 import java.util.Observer;
-import java.util.Properties;
 
 import javax.swing.JFrame;
 
 /**
- * Clase para dibujar la calculadora
- *
+ * Clase que obsverva a la clase {@link CalculadoraLineal} para dibujar la calculadora
+ * TODO
+ * Hacer andar el caso=0.3+0.3
  */
 
 public class CalculadoraGrafica extends JFrame implements Observer
 {
 	private static final long serialVersionUID = 1L;
-	private  static final int anchoButton = 35;
-	private  static final int altoButton = 35;
-	private CalculadoraLineal calc;
+	private static final int anchoButton = 35;
+	private static final int altoButton = 35;
 	private final TextField txt = new TextField();
+	private CalculadoraLineal calc = new CalculadoraLineal();
+	private String lastActionPerformed = new String();
 	
 	public CalculadoraGrafica()
 	{
 		super("Calculadora");
+		calc.addObserver(this);
 		crearControles();
 	}
 	
 	@Override
-	public void update(Observable arg0, Object arg1){}
+	public void update(Observable who, Object what)
+	{
+		CalculadoraLineal calc = (CalculadoraLineal) who;
+		txt.setText(String.valueOf(calc.getTotal()));
+	}
 	
 	public void crearVentana()
 	{
@@ -62,6 +64,7 @@ public class CalculadoraGrafica extends JFrame implements Observer
 		txt.setBackground(Color.WHITE);
 		txt.setForeground(Color.BLACK);
 		txt.setEnabled(false);
+		txt.setText("0");
 	}
 	
 	public void mostrarNumeros()
@@ -69,7 +72,7 @@ public class CalculadoraGrafica extends JFrame implements Observer
 		Button btn;
 		int y=101;
 		Integer numero=1;
-
+		
 		//1 al 9
 		for(int i=0;i<3;i++)
 		{
@@ -80,54 +83,24 @@ public class CalculadoraGrafica extends JFrame implements Observer
 				add(btn);
 				btn.setName("numero"+String.valueOf(numero));
 				btn.setBounds(1+(j*37),y,anchoButton,altoButton);
-				btn.addKeyListener(new KeyAdapter() 
-				{
-					@Override
-					public void keyPressed(KeyEvent e) 
-					{
-						super.keyPressed(e);
-						//Si es un número lo ingresa en el textfield
-						if(e.getKeyChar() >= '0' && e.getKeyChar() <= '9')
-						{
-							txt.setText(txt.getText()+  e.getKeyChar() );
-						}
-					}
-				});
-				btn.addActionListener( new ActionListener() 
-				{					
-					@Override
-					public void actionPerformed(ActionEvent e)
-					{
-						txt.setText(txt.getText()+  valor );
-					}
-				}
-				);
+				btn.addActionListener(new NumberEventHandler(txt,valor));
 				numero++;
 			}
 			y=y-37;
 		}
+		/**
+		 * TODO Validar que solo se pueda escribir uno, salvo si hay una coma
+		 */
 		//0
 		btn = new Button("0");
 		add(btn);
-		btn.addKeyListener(new KeyAdapter() 
-		{
-			@Override
-			public void keyPressed(KeyEvent e) 
-			{
-				super.keyPressed(e);
-				//Si es un número lo ingresa en el textfield
-				if(e.getKeyChar() >= '0' && e.getKeyChar() <= '9')
-				{
-					txt.setText(txt.getText()+  e.getKeyChar() );
-				}
-			}
-		});
 		btn.addActionListener( new ActionListener()
 		{	
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
 				txt.setText( txt.getText() + "0");
+				lastActionPerformed="number";
 			}
 		});
 		btn.setName("numero"+String.valueOf(0));
@@ -148,10 +121,21 @@ public class CalculadoraGrafica extends JFrame implements Observer
 	{
 		//Coma
 		Button btn;
-		btn = new Button(",");
+		btn = new Button(".");
 		add(btn);
 		btn.setName("coma");
 		btn.setBounds(75,138,anchoButton,altoButton);
+		btn.addActionListener(new ActionListener()
+		{	
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				//Valido que solo se pueda escribir una sola
+				if((txt.getText()).indexOf(".")==-1)
+					txt.setText(txt.getText()+  "." );
+				lastActionPerformed=".";
+			}
+		});
 	}
 	
 	public void mostrarCE()
@@ -162,6 +146,17 @@ public class CalculadoraGrafica extends JFrame implements Observer
 		add(btn);
 		btn.setName("ce");
 		btn.setBounds(1,175,109,altoButton);
+		btn.addActionListener(new ActionListener()
+		{	
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				calc.limpiar();
+				//Sino hago esto me pone 0.0
+				txt.setText("0");
+				lastActionPerformed="CE";
+			}
+		});
 	}
 	
 	public void mostrarIgual()
@@ -172,6 +167,15 @@ public class CalculadoraGrafica extends JFrame implements Observer
 		add(btn);
 		btn.setName("igual");
 		btn.setBounds(111,175,anchoButton,altoButton);
+		btn.addActionListener(new ActionListener()
+		{	
+			@Override
+			public void actionPerformed(ActionEvent arg0)
+			{
+				calc.agregarNumero(Double.valueOf(txt.getText()));
+				lastActionPerformed="=";
+			}
+		});
 	}
 	
 	public void mostrarOperaciones()
@@ -182,32 +186,25 @@ public class CalculadoraGrafica extends JFrame implements Observer
 		add(btn);
 		btn.setName("suma");
 		btn.setBounds(111,27,anchoButton,altoButton);
-		btn.addActionListener( new ActionListener()
-		{	
-			@Override
-			public void actionPerformed(ActionEvent e)
-			{
-				/*calc.agregarNumero(Double.valueOf(txt.getText()));
-				calc.agregarOperacion("+");
-				txt.setText(String.valueOf(calc.getTotal()));
-				*/
-			}
-		});
+		btn.addActionListener(new OperationEventHandler());
 		
 		btn = new Button("-");
 		add(btn);
 		btn.setName("resta");
 		btn.setBounds(111,64,anchoButton,altoButton);
+		btn.addActionListener(new OperationEventHandler());
 		
 		btn = new Button("*");
 		add(btn);
 		btn.setName("multiplicacion");
 		btn.setBounds(111,101,anchoButton,altoButton);
+		btn.addActionListener(new OperationEventHandler());
 		
 		btn = new Button("/");
 		add(btn);
 		btn.setName("division");
 		btn.setBounds(111,138,anchoButton,altoButton);
+		btn.addActionListener(new OperationEventHandler());
 	}
 	
 	public void mostrarBotones()
@@ -226,5 +223,63 @@ public class CalculadoraGrafica extends JFrame implements Observer
 		crearTextField();
 		mostrarBotones();
 	}
-
+	
+	/**
+	 * Clase para el manejo de los eventos de los botones del 1 al 9
+	 * @author MCUPO
+	 */
+	
+	private class NumberEventHandler implements ActionListener 
+	{
+		private final TextField txt;
+		private final String valor;
+		
+		/**
+		 * @param textfield - Display de la calculadora
+		 * @param val - Valor que representa el botón
+		 */
+		NumberEventHandler(TextField textfield, String val)
+		{
+			txt = textfield;
+			valor = val;
+		}
+		@Override
+		public void actionPerformed(ActionEvent arg0) 
+		{
+			//Pregunto si hay un cero o si el último botón presionado fue el de una operación
+			if((txt.getText().equals("0")) || (lastActionPerformed.equals("operation")))
+			{
+				txt.setText(valor);
+			}
+			else
+			{	
+				txt.setText(txt.getText()+  valor );
+			}
+			lastActionPerformed="number";
+		}
+	}
+	
+	/**
+	 * Clase para el manejo de los botones de las operaciones
+	 * @author MCUPO
+	 */
+	
+	private class OperationEventHandler implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			//Si el último botón presionado fue el de resultado, no agrego el número
+			if(lastActionPerformed.equals("="))
+			{
+				calc.agregarOperacion(((Button)e.getSource()).getLabel());
+			}
+			else
+			{
+				calc.agregarNumero(Double.valueOf(txt.getText()));
+				calc.agregarOperacion(((Button)e.getSource()).getLabel());
+			}
+			lastActionPerformed="operation";
+		}
+	}
 }
