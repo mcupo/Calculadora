12f1433e19b3e520e124a8ab694f885831fcc53a
MCUPO <MCUPO@e0e46c49-be69-4f5a-ad62-21024a331aea>
2012-05-21 20:11:10 +0000

    TP2 terminado

---
+++ Java/T6/TP2/src/ar/edu/ort/operaciones/Raiz.java
@@ -0,0 +1,11 @@
+package ar.edu.ort.operaciones;
+import ar.edu.ort.calculadora.Operacion;
+
+public class Raiz extends Operacion
+{
+	@Override
+	public double calcular(double numero1, double numero2)
+	{
+		return Math.sqrt(numero1);
+	}
+}
